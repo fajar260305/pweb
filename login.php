64a89d42087f59d2b34bb41a4f08ad664344ae3a
faiz r <faizramadhan021104@gmail.com>
2022-05-21 23:11:16 +0700

    benah

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
 
@@ -17,7 +16,7 @@
     <div class="global-container">
         <div class="card login-form">
             <div class="say">
-                <h1 class="card-title text-center">REGISTER</h1>
+                <h1 class="card-title text-center">LOGIN</h1>
             </div>
             <div class="card-text">
                 <form>
@@ -29,8 +28,12 @@
                         <label for="exampleInputPassword1" class="form-label">Password</label>
                         <input type="password" class="form-control" id="exampleInputPassword1">
                     </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Ingatkan saya</label>
+                    </div>
                     <p class="mb-0">
-                         <a href="add-user.php" class="text-center">Sing in</a>
+                        <a href="register.php" class="text-center">Sign in</a>
                     </p>
                     <div class="d-grid gap-2">
                         <button type="submit" class="btn btn-primary">Masuk</button>
