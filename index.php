9343730e8fc5e4ea4d7ce798ac17df36e4fb22bb
fajarusshodik <fajarrusshodik@gmail.com>
2022-05-21 15:53:10 +0700

    gas

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
+</head>
+<body>
+
+
+
+<!-- start navbar -->
+<nav class="navbar navbar-expand-lg navbar-light ">
+  <div class="container-fluid">
+  <a class="mx-3"  href="#">
+    <img src="foto/win.jpg" alt="siap" width="60" height="60">
+    </a>
+    <a class="navbar-brand" href="#">SMK negeri 1 Cirebon</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- end navbar -->
+
+
+<div class="container-fluid ">
+  <div class="row align-items-start">
+    <div class="col text-center" style="margin-top: 80px;">
+      <p class="h1 m-5" style="font-size: 50px;">Selamat datang di website kami</p>
+    </div>
+  </div>
+  <div class="row align-center">
+    <div class="col">
+        <p class="col text-center">Smk Negeri 1 Cirebon trus melangkah maju untuk menjalin kerjasama dengan dunia industri, guna memberikan lulusan SMK yang kompeten dan bisa bekerj di perusahaan</p>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div class="container-fluid">
+  <div class="row align-items-start">
+    <div class="col text-center mt-3 border">
+        <span class="border border-primary mx-2"><span class="m-3">TGB</span></span>
+        <span class="border border-secondary mx-2"><span class="m-3">FGG</span></span>
+        <span class="border border-success mx-2"><span class="m-3">YTR</span></span>
+        <span class="border border-danger mx-2"><span class="m-3">TPM</span></span>
+        <span class="border border-warning mx-2"><span class="m-3">RPL</span></span>
+    </div>
+  </div>
+</div>
+
+
+<script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
