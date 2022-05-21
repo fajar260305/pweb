d26cc8fa187e674701a1e6bfde868fa999e21f85
faiz r <faizramadhan021104@gmail.com>
2022-05-21 18:42:35 +0700

    Faiz

--- index.php
+++ index.php
@@ -1,102 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<link>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
+  <link>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
 </head>
+
 <body>
 
 
 
-<!-- start navbar -->
-<nav class="navbar navbar-expand-lg navbar-light ">
-  <div class="container-fluid">
-  <a class="mx-3"  href="#">
-    <img src="foto/win.jpg" alt="siap" width="60" height="60">
-    </a>
-    <a class="navbar-brand" href="#">SMK negeri 1 Cirebon</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="#">profil</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            absen
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Guru</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Siswa</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active">kontak</a>
-        </li>
-      </ul>
+  <!-- start navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container-fluid">
+      <a class="mx-3" href="#">
+        <img src="foto/win.jpg" alt="siap" width="60" height="60">
+      </a>
+      <a class="navbar-brand" href="#">SMK negeri 1 Cirebon</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">profil</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              absen
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Guru</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Siswa</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active">kontak</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- end navbar -->
+
+
+
+  <!-- start konten 1 -->
+  <div class="container-fluid ">
+    <div class="row align-items-start">
+      <div class="col text-center" style="margin-top: 100px;">
+        <p class="h1 m-2" style="font-size: 50px;">Selamat datang di website kami</p>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-4 text-center">
+        Smk Negeri 1 Cirebon trus melangkah maju untuk menjalin kerjasama dengan dunia industri, guna memberikan lulusan SMK yang kompeten dan bisa bekerj di perusahaan
+      </div>
     </div>
   </div>
-</nav>
-<!-- end navbar -->
+  <!-- start konten 1 -->
 
 
 
-<!-- start konten 1 -->
-<div class="container-fluid ">
-  <div class="row align-items-start">
-    <div class="col text-center" style="margin-top: 100px;">
-      <p class="h1 m-2" style="font-size: 50px;">Selamat datang di website kami</p>
-    </div>
-  </div>
-  <div class="row justify-content-center">
-    <div class="col-4 text-center">
-    Smk Negeri 1 Cirebon trus melangkah maju untuk menjalin kerjasama dengan dunia industri, guna memberikan lulusan SMK yang kompeten dan bisa bekerj di perusahaan
-    </div>
-  </div>
-</div>
-<!-- start konten 1 -->
-
-
-
-<!-- start konten 2 -->
-<div class="container-fluid mt-3">
-  <div class="row align-items-start">
-    <div class="col text-center mt-3 ">
+  <!-- start konten 2 -->
+  <div class="container-fluid mt-3">
+    <div class="row align-items-start">
+      <div class="col text-center mt-3 ">
         <span class="border border-primary mx-2"><span class="m-3">TGB</span></span>
         <span class="border border-secondary mx-2"><span class="m-3">TKR</span></span>
         <span class="border border-success mx-2"><span class="m-3">TOI</span></span>
         <span class="border border-danger mx-2"><span class="m-3">TPM</span></span>
         <span class="border border-warning mx-2"><span class="m-3">RPL</span></span>
         <span class="border border-warning mx-2"><span class="m-3">TKJ</span></span>
+      </div>
     </div>
   </div>
-</div>
-<!-- end konter 2 -->
+  <!-- end konter 2 -->
 
-<!-- start button -->
-<div class="container-fluid ">
-  <div class="row align-items-start">
-    <div class="col text-center mt-5">
-    <a href="add-user.php"><button type="button" class="btn btn-outline-primary" style="margin-top: 20px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-    </svg> Get start</button></a>
+  <!-- start button -->
+  <div class="container-fluid ">
+    <div class="row align-items-start">
+      <div class="col text-center mt-5">
+        <a href="login.php"><button type="button" class="btn btn-outline-primary" style="margin-top: 20px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+            </svg> Get start</button></a>
+      </div>
     </div>
   </div>
-</div>
-<!-- end buttom -->
+  <!-- end buttom -->
 
 
 
-<script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js" ></script>
+  <script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,32 +1,53 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-<link>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="bootstrap-5.1.3-dist/css/style.css" rel="stylesheet">
+  <title>Form Login</title>
 </head>
+
 <body>
+  <div class="global-container">
+    <div class="card login-form">
+      <div class="card-body">
+        <h1 class="card-title text-center">LOGIN</h1>
 
-<form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
 
-<script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js" ></script>
+      </div>
+      <div class="card-text">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Ingatkan saya</label>
+          </div>
+          <p class="mb-0">
+            <a href="add-user.php" class="text-center">Daftarkan Segera Akun Mu disini</a>
+          </p>
+          <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+
+
 </body>
+
 </html>
