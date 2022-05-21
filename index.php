20c23b4ee26560c53909991223a62e37f81234fe
fajarusshodik <fajarrusshodik@gmail.com>
2022-05-21 16:42:11 +0700

    bisa

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 <!-- start konten 2 -->
 <div class="container-fluid mt-3">
   <div class="row align-items-start">
-    <div class="col text-center mt-3 border">
+    <div class="col text-center mt-3 ">
         <span class="border border-primary mx-2"><span class="m-3">TGB</span></span>
         <span class="border border-secondary mx-2"><span class="m-3">TKR</span></span>
         <span class="border border-success mx-2"><span class="m-3">TOI</span></span>
@@ -82,6 +82,19 @@
   </div>
 </div>
 <!-- end konter 2 -->
+
+<!-- start button -->
+<div class="container-fluid ">
+  <div class="row align-items-start">
+    <div class="col text-center mt-5">
+    <a href="add-user.php"><button type="button" class="btn btn-outline-primary" style="margin-top: 20px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+    </svg> Get start</button></a>
+    </div>
+  </div>
+</div>
+<!-- end buttom -->
+
 
 
 <script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js" ></script>
