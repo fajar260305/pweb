a341a405a799425b17ff6b5830ac3be842b0c52e
fabian <fabianrifqi21@gmail.com>
2022-05-22 18:18:22 +0700

    membuat halaman absen guru

---
+++ absen-siswa.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <title>Document</title>
+    <style>
+        .container {
+        margin-top: 20vh;
+        }
+
+        .card {
+            box-shadow: 0 -3px cornflowerblue;
+        }
+
+        .card-title {
+            margin-bottom: 5vh;
+        }
+
+        .card-body .jam {
+            color: #1d59f2;
+            background-color: #2d2d2e;
+        }
+
+        .card-title {
+            font-family: verdana, sans-serif;
+            font-weight: 900;
+            padding-top: 20px;
+            color: cornflowerblue;
+        }
+
+        h3 {
+            margin-bottom: 5vh;
+        }
+    </style>
+    <script>
+    setInterval(() => {
+        let date = new Date()
+        let clock = document.getElementById('clock')
+        clock.innerHTML =
+            date.getHours()+ " : " +
+            date.getMinutes()+ " : " +
+            date.getSeconds()
+    }, 1000);
+    </script>
+</head>
+<body>
+<div class="container bg-body text-dark">
+    <div class="row justify-content-center">
+        <div class="col-4 mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <form action="">
+                        <h1 class="card-title text-center">Absen</h1>
+                        <div class="jam">
+                        <h3 class="text-center" id="clock"></h3>
+                        </div>
+                        <!-- jurusan -->
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Pilih Jurusan</option>
+                            <option value="1">XI TGB</option>
+                            <option value="2">XI TKR</option>
+                            <option value="3">XI TOI</option>
+                            <option value="4">XI TPM</option>
+                            <option value="5">XI TKR</option>
+                            <option value="6">XI TKJ</option>
+                            <option value="7">XI RPL</option>
+                        </select>
+
+                        <!-- absen -->
+                        <div class="mt-3 d-flex justify-content-around">
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                                Hadir
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                            <label class="form-check-label" for="flexRadioDefault2">
+                                Izin
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                            <label class="form-check-label" for="flexRadioDefault2">
+                                Alfa
+                            </label>
+                        </div>
+                        </div>
+                        <div class="mt-5 text-center">
+                            <button type="button" class="btn btn-primary">Kirim</button>
+                        </div>  
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+
+<script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -28,20 +28,20 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="#">profil</a>
+          <a class="nav-link active" href="#">Profil</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            absen
+            Absen
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Guru</a></li>
+            <li><a class="dropdown-item" href="absen-guru.php">Guru</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="absen.php">Siswa</a></li>
+            <li><a class="dropdown-item" href="absen-siswa.php">Siswa</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link active">kontak</a>
+          <a class="nav-link active">Kontak</a>
         </li>
       </ul>
     </div>
